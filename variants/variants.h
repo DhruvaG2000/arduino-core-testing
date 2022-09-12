b3a7ada960e64e870289fd7342da6509203363cd
Mike Szczys <mike@golioth.io>
2022-09-11 21:47:32 -0500

    variants: add nrf52840dk
    Add varints folder and pinmapping for nRF52840dk
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- variants/variants.h
+++ variants/variants.h
@@ -7,3 +7,6 @@
 #ifdef CONFIG_BOARD_ARDUINO_NANO_33_IOT
 #include <arduino_nano_33_iot_pinmap.h>
 #endif // CONFIG_BOARD_ARDUINO_NANO_33_IOT
+#ifdef CONFIG_BOARD_NRF52840DK_NRF52840
+#include "nrf52840dk_nrf52840_pinmap.h"
+#endif // CONFIG_BOARD_NRF52840DK_NRF52840
