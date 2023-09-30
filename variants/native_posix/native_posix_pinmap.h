e2a18d03b1e960a06ecedce20baed3dcc30214cb
Dhruva Gole <goledhruva@gmail.com>
2023-10-01 00:00:57 +0530

    variants: Add native_posix variant support
    Add support for native_posix in limited capacity
    
    Signed-off-by: Dhruva Gole <goledhruva@gmail.com>

---
+++ variants/native_posix/native_posix_pinmap.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2022 Dhruva Gole
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/* All the pins that are 100 + x are gpio1 pins and < 100 are in gpio0 */
+#pragma once
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+
